cc9e66fd813b929a6e32223e02eef5218a6f228c
Christoffer <chris3johansson@gmail.com>
2025-03-21 09:42:19 +0100

    small

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
 
     def __repr__(self):
